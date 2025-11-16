e5399d0078a00452b94a3091283e3ba47058a134
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-11-16 19:22:59 +0200

    Advanced Task

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonProps } from "@/interfaces";
+const Button = ({ title, styles }) => {
+  return (
+    <button
+      className={` border px-3 py-1  lg:text-lg  md:text-md sm:text-sm lg:rounded-b-lg md:rounded-b-md sm:rounded-b-sm  mt-3  bg-blue-500 text-white rounded-full  ${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+export default Button;
